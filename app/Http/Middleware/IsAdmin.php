f0f1e6e68032d39949c37af948dc25b25ef3c474
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2025-01-12 09:47:52 +0100

    mostly setting up routes and admin/user security

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth; // Import Auth facade cuz ide not happe w auth()->user()
+
+
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+{
+    if (Auth::check() && Auth::user()->role !== 'admin') {
+        return redirect('/'); // Redirect non-admins
+    }
+    return $next($request);
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::middleware('isAdmin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
+});
 require __DIR__.'/auth.php';
